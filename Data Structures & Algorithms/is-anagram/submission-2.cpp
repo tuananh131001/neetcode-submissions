667b0ab94f0f84d668a30e33f3ad9a7387708dac
Anh Nguyen <tuananh131001@gmail.com>
2026-08-05 22:35:07 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size() ) return false;
+        unordered_map<char, int>  charS;
+        unordered_map<char, int> charT;
+        for (int i= 0; i < s.size(); i++) {
+            charS[s[i]]++;
+            charT[t[i]]++;
+        }
+
+        return charS == charT;
+    }
+};
