db85255bd5dd8f7d4da4a838f9cff444195e018e
Anh Nguyen <tuananh131001@gmail.com>
2026-08-08 22:31:39 +0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char,int> charS;
+        unordered_map<char,int> charT;
+        for (int i = 0 ; i < s.size(); i++) {
+            charS[s[i]]++;
+            charT[t[i]]++;
+        }
+        return charS == charT;
+    }
+};
