fcec8f4cd3c555acffeefe5eb1186b6e4ef655a2
Anh Nguyen <tuananh131001@gmail.com>
2026-08-12 10:21:42 +0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int COLS = matrix.size();
+        int ROWS = matrix[0].size();
+
+        int top = 0, bot = COLS - 1;
+        while (top <= bot) {
+            int midIdx = top + (bot - top) / 2;
+            if (target > matrix[midIdx][ROWS - 1]) {
+                top = midIdx + 1; 
+            } else if (target < matrix[midIdx][0]) {
+                // [[0, 2, 4], [5, 7, 9]] // target = 2 , first 2nd row is 5
+                bot = midIdx - 1;
+            } else {
+                break;
+            }
+        }
+
+        if (!(top <= bot)) {
+            return false;
+        }
+
+        int row = (top + bot) / 2; // ??
+        cout << "res at row: " << row << endl;
+        int l = 0, r = ROWS - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (matrix[row][m] == target) {
+                return true;
+            }
+            // [0,  2 , 4 , 5] target 2 , mid = 4
+            // [10, 11, 12, 13] target 10
+            // matrix[row][m] = 12;
+            // 12 > target -> l = 2 + 1 [Wrong] - > m - 1
+            // [10 , 11] mid = 10,5 = 11 > 
+            else if (matrix[row][m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return false;
+    }
+};
