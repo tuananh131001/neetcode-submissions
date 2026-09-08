74062b72f9206d2e09a5e04b2f2fdefb7314bf53
Anh Nguyen <tuananh131001@gmail.com>
2026-09-08 10:22:47 +0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // how to detect if the num is the begging of starting sequence
+        // use hashset and check if the num - 1 is already in the set then start an while loop to calc
+        set<int> pastSet = set(nums.begin(), nums.end());
+        int res = 0;
+
+        for (int num: nums) { // num = 3 , num = 5
+            if (pastSet.find(num - 1) != pastSet.end()) { // found // found num = 2, num = 5 is in set
+                int length = 1;
+                while (pastSet.find(num  - 1 + length) != pastSet.end()) // continue loop bit start from the num which start the sequece . My error is start from the current num which not the start of sequence so the length max is not the length of the sequence
+                    length++;
+                res = max(length, res); // res = 4
+            }
+        }
+        return res;
+    }
+};
