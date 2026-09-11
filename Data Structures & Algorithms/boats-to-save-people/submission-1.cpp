6e84e3c6014a7f0d83fdb037bce7d4e218bd0bfe
Anh Nguyen <tuananh131001@gmail.com>
2026-09-11 15:19:38 +0700

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        // [1,2,4,5]
+        // if sum >= limit -> move right and increase length
+        // else move left
+        // 1 + 5 = limit 
+        // 1 + 4 -> move left
+        // 2 + 4 -> move right and increase lenght
+
+        // [3,5,3,4]
+        // first boat: [4]
+        // second boat: [3]
+        // three boat: [5]
+        // fource: [3]
+        // possible pattern in two pointer
+        // 1. Left and Right pointer
+        // 2. Fast and slow pointer
+        // combine those method with hash, set, stack, queue?
+        // init an slow and fast pointer
+        // fast pointer will check if the next number of people is fit to an boat else increase the length
+
+        // [5,1,4,2]
+
+        sort(people.begin(), people.end());
+        int l = 0;
+        int r = people.size() - 1;
+        int res = 0;
+        while (l < r) {
+            int sum = people[l] + people[r];
+            if (sum >= limit) {
+                res += 1;
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return res;
+    }
+};
