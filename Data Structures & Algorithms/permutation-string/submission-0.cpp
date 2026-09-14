a3d672b8a3ccba8df729cb20d7763588fff4dac1
Anh Nguyen <tuananh131001@gmail.com>
2026-09-14 11:45:13 +0700

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        // what is an permutation
+        // check if char in s1 (hashset) then 
+        // s1 = "eeba"
+        // s2 = "lecabe"
+        [{e, 2}, {b ,1}, {a, 1}]
+        l
+        e -> [{e, 1}, {b , 1}, {a ,1}]
+        ec -> not found
+        ca -> 
+
+        // Set([eeba])
+        l
+        e -> found start win
+        ec -> not , reset
+        a -> yes
+        ab -> yes
+        e -> yes count = 3
+
+        /// hashmap, and hashmap for permu
+        // hash1 = [{e, 2}, {b ,1}, {a, 1}]
+
+        l -> not in skip
+        e -> in creates hash [{e , 1}]
+        
+         
+
+
+
+        
+    }
+};
